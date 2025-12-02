b8f945dfec09a9fc392b0bcfce4f26550e67b7f1
Rasyid Nabil P <rasyidnabil06@gmail.com>
2025-12-03 05:12:38 +0700

    change: remove CategoryDropdown for a while: error build

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import CategoryDropdown from "../CategoryDropdown";
+// import CategoryDropdown from "../CategoryDropdown";
 
 const navItems = [
    { path: "/home", label: "Home" },
